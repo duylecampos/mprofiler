65bd228cfa2afa017aad2b7f0f1ba6e9dbfd5f39
Duyle Campos <duylecampos@gmail.com>
2017-10-25 00:37:01 -0200

    Update viewer to show expected format
    Update viewer to convert seconds to minutes

--- lib/mprofiler/viewer.rb
+++ lib/mprofiler/viewer.rb
@@ -3,20 +3,27 @@ module Mprofiler
         def self.format(data)
             tree = { 
                 'local' => {
-                    'cellphone' => prepare_data(data['local_cellphone']),
-                    'phone' => prepare_data(data['local_phone']),
+                    'cellphone' => prepare_data(data['local_cellphone'], format='minutes'),
+                    'phone' => prepare_data(data['local_phone'], format='minutes'),
                 }, 
                 'long_distance' => {
-                    'cellphone' => prepare_data(data['long_distance_cellphone']),
-                    'phone' => prepare_data(data['long_distance_phone']),
+                    'cellphone' => prepare_data(data['long_distance_cellphone'], format='minutes'),
+                    'phone' => prepare_data(data['long_distance_phone'], format='minutes'),
                 }, 
-                'sms' => prepare_data(data['sms']),
+                'sms' => prepare_data(data['sms'], format=false),
                 'internet' => prepare_data(data['internet']),
             }
         end
 
     private 
-        def self.prepare_data(data)
+        def self.prepare_data(data, format=nil)
+            if format == false
+                return data
+            end
+            data = data.to_f
+            if format == 'minutes'
+                data = data / 60
+            end
             data.round(2)
         end
     end
--- spec/viewer_spec.rb
+++ spec/viewer_spec.rb
@@ -4,26 +4,25 @@ require 'csv'
 describe Mprofiler::Viewer do
     it 'format data to show' do
         raw_data = {
-            'local_cellphone' => 0,
-            'local_phone' => 1.9859,
-            'long_distance_cellphone' => 2.485,
-            'long_distance_phone' => 3,
+            'local_cellphone' => 150,
+            'local_phone' => 180,
+            'long_distance_cellphone' => 100,
+            'long_distance_phone' => 300,
             'sms' => 4,
-            'internet' => 5,
+            'internet' => 5.8989887,
         }
         data_to_display = Mprofiler::Viewer.format raw_data
         expect(data_to_display).to match({
             'local' => {
-                'cellphone' => 0,
-                'phone' => 1.99,
-
+                'cellphone' => 2.5,
+                'phone' => 3.0,
             },
             'long_distance' => {
-                'cellphone' => 2.49,
-                'phone' => 3,
+                'cellphone' => 1.67,
+                'phone' => 5.0,
             },
             'sms' => 4,
-            'internet' => 5,
+            'internet' => 5.9,
         })
     end
 end
